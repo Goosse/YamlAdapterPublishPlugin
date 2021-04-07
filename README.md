880a9a13122a4cb0ba1fe5c4fdd839c4044f8908
Joshua Goossen <jigoossen@gmail.com>
2021-04-06 22:31:30 -0300

    Basics working

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # YamlAdapterPublishPlugin
 
-A description of this package.
+This "tweaks" YAML metadata (exported by 3rd party tools such as Netlify CMS) to Publish's native format. 
--- Sources/YamlAdapterPublishPlugin/YamlAdapterPublishPlugin.swift
+++ Sources/YamlAdapterPublishPlugin/YamlAdapterPublishPlugin.swift
@@ -1,3 +1,27 @@
-struct YamlAdapterPublishPlugin {
-    var text = "Hello, World!"
+import Ink
+import Publish
+import Foundation
+//import Yams
+
+public extension Plugin {
+    static func yamlAdapter() -> Self {
+        Plugin(name: "YAML Adapter") { context in
+            context.markdownParser.addModifier(
+                .removeQuotes()
+            )
+        }
+    }
+}
+
+public extension Modifier {
+    static func removeQuotes() -> Self {
+
+        return Modifier(target: .metadataValues) { html, metadata in
+        
+            let removedSingleQuotes = html.trimmingCharacters(in: CharacterSet(charactersIn: "\'"))
+            print(removedSingleQuotes.trimmingCharacters(in: CharacterSet(charactersIn: "\"")));
+            return removedSingleQuotes.trimmingCharacters(in: CharacterSet(charactersIn: "\""))
+
+        }
+    }
 }
